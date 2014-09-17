a825675bc325601c6b50b05bbd271a833380154b
Sara Collini <scolli16@masonlive.gmu.edu>
2014-09-17 13:55:15 -0400

    Added new functions

--- datamodel.js
+++ datamodel.js
@@ -20,6 +20,14 @@ function slave(first, midwife, date, amount, farm) {
     this.date = date;
     this.amount = amount;               
     this.farm = farm;
+    this.toString = function() {
+      return this.midwife + " was paid " + this.amount  + " in " + this.date + " to deliver " + this.first + " at " + this.farm;
+
+    },
+    this.print = function() {
+      console.log(this.midwife + " was paid " + this.amount  + " in " + this.date + " to deliver " + this.first + " at " + this.farm);
+
+    }
 }
  /*These are my new slave objects*/
 var Moll = new slave("Moll", "Susanna Bishop", 1771, "10s", "Unknown");
@@ -27,8 +35,22 @@ var Betty = new slave("Betty", "Susanna Bishop", 1771, "10s", "Unknown");
 var Alce = new slave("Alce", "Susanna Bishop", 1771, "10s", "Mansion House farm");
 var Doll = new slave("Doll", "Susanna Bishop", 1772, "10s", "Unknown");
 var Sall = new slave("Sall", "Susanna Bishop", 1772, "10s", "Mansion House farm");
+var SlaveObjects = [];
+SlaveObjects.push(Moll, Betty, Alce, Doll, Sall);
+console.log(SlaveObjects);
 
-console.log(Alce.midwife + " was paid " + Alce.amount  + " in " + Alce.date + " to deliver " + Alce.first + " at " + Alce.farm) 
-document.write(Alce.midwife + " was paid " + Alce.amount  + " in " + Alce.date + " to deliver " + Alce.first + " at " + Alce.farm);
+document.write("<ul>")
+for(var i = 0; i < SlaveObjects.length; i++) {
+  document.write(list(SlaveObjects[i].toString()));
+
+}
+document.write("</ul>")
+//console.log(Alce.midwife + " was paid " + Alce.amount  + " in " + Alce.date + " to deliver " + Alce.first + " at " + Alce.farm) 
+//document.write(Alce.midwife + " was paid " + Alce.amount  + " in " + Alce.date + " to deliver " + Alce.first + " at " + Alce.farm);
 
 //Susanna Bishop was paid 10s in 1771 to deliver Alce at Mansion House farm
+
+function list(string) {
+  return "<li>" + string + "</li>";
+
+}
